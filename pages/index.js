6b180a2de756d89d9aaf9a9f442b19418839360c
lee14k <kmkovach@mtu.edu>
2024-02-22 11:50:36 -0500

    linking buttons

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,10 @@ export default function Home() {
         <div className="sm:w-1/4 flex justify-center items-center mt-96 ">
           <div className={wellfleet.className}>
             <div className="text-white  bg-emerald-800 flex justify-center items-center circletwo">
-            
               <span className="text-center mx-12">
                 Handcrafted burgers and pizzas inspired by gourmet northern
                 tastes
               </span>
-            
             </div>
           </div>
         </div>
@@ -42,17 +40,17 @@ export default function Home() {
           {/* Adjusted to align the buttons vertically */}
           <Link href="https://www.toasttab.com/lakers">
             <button className="w-full h-full bg-emerald-800 rounded-md px-3.5 py-2.5 text-l font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              Order Now!
+              <span className={wellfleet.className}>Order Now!</span>
             </button>
           </Link>
-          <Link href="https://orders.cake.net/11333695">
+          <Link href="#menu">
             <button className="w-full h-full bg-emerald-800 rounded-md px-3.5 py-2.5 text-l font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              Menu
+              <span className={wellfleet.className}>Menu</span>
             </button>
           </Link>
-          <Link href="https://orders.cake.net/11333695">
+          <Link href="#contact">
             <button className="w-full h-full bg-emerald-800 rounded-md px-3.5 py-2.5 text-l font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              Contact Us
+              <span className={wellfleet.className}>Contact Us</span>
             </button>
           </Link>
         </div>
@@ -64,11 +62,14 @@ export default function Home() {
           </h1>
         </div>
       </div>
-      <div className="">
+      <div id="menu" className="">
         <Menu />
       </div>
       <div className={wellfleet.className}>
-        <div className="bottomcontact flex flex-col justify-center items-center pt-12">
+        <div
+          id="contact"
+          className="bottomcontact flex flex-col justify-center items-center pt-12"
+        >
           <h1 className="text-white text-4xl">Contact Us</h1>
           <ContactForm />
         </div>
