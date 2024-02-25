95c3a2a2850fce86bd32f1e4d80e87f48b29e8e0
lee14k <kmkovach@mtu.edu>
2024-02-25 18:38:33 -0500

    mobile stuff

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,9 +1,9 @@
 export default function Menu() {
   return (
     <div className="py-2 menucontainer">
-      <div className="grid grid-cols-1 sm:grid-cols-2">
-        <div className="menu-one"></div>
-        <div className="menu-two"></div>
+      <div className="flex flex-col sm:grid grid-cols-2">
+        <div className="h-1/2 w-full menu-one"></div>
+        <div className="h-1/2 w-full menu-two"></div>
       </div>
     </div>
   );
